a08c774a4b65057c5f1e131ee12ed38f48983e7c
nagyerik99 <nagyerik99@gmail.com>
2022-04-22 09:33:24 +0200

    Random generator changed to SecureRandom

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -1,5 +1,6 @@
 package hu.bme.mit.spaceship;
 
+import java.security.SecureRandom;
 import java.util.Random;
 
 /**
@@ -14,7 +15,7 @@ public class TorpedoStore {
 
   private int torpedoCount = 0;
 
-  static Random generator = new Random();
+  static SecureRandom generator = new SecureRandom();
 
   public TorpedoStore(int numberOfTorpedos){
     this.torpedoCount = numberOfTorpedos;
